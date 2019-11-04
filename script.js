c0783f9f57c5fa3cafd2abc609faa7a3869dc897
ごきげんよう <xfmniao@googlegroups.com>
2019-11-04 13:39:24 +0800

    backup

---
+++ script.js
@@ -0,0 +1,22 @@
+
+	<script>
+	var theme_options = {
+		theme_developer      :"xfmniao@yahoo.co.jp",
+		theme_type           :'column',
+		theme_name           :'iprgara',
+		theme_number         :8,
+		color_background_rgb     :'{RGBcolor:Body background}',
+		infinite_scroll      :{block:IfInfiniteScroll}true,{/block:IfInfiniteScroll}
+                          {block:IfNotInfiniteScroll}false,{/block:IfNotInfiniteScroll}
+		infinite_scroll_str  :"{lang:No more posts}",
+		form_photosets       :{block:IfFormPhotosets}true,{/block:IfFormPhotosets}
+		                      {block:IfNotFormPhotosets}false,{/block:IfNotFormPhotosets}
+		widget_grid          :true,
+		photoset_gap         :'20px',
+		attribution_style    :'{select:Theme attribution style}',
+		menu                 :'{select:Menu}',
+		make_it_snow         :{block:IfMakeItSnow}true{/block:IfMakeItSnow}
+                          {block:IfNotMakeItSnow}false{/block:IfNotMakeItSnow}
+	};
+	</script>
+	
